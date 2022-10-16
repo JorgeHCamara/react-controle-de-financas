1deaf642b273c5d55e7c1984bb5072dcc2389b84
Jorge Camara <jcamara@HYPERATIVA.NET>
2022-10-16 03:04:15 -0300

    teste

---
+++ src/components/Resume/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ResumeItem from "../ResumeItem";
+import * as C from "./styles";
+import { FaRegArrowAltCircleUp, FaRegArrowAltCircleDown, FaDollarSign } from "react-icons/fa";
+
+export default function Resume({ income, expense, total }) {
+
+    return(
+        <C.Container>
+            <ResumeItem title="Entradas" Icon={FaRegArrowAltCircleUp} value={income} />
+            <ResumeItem title="Saídas" Icon={FaRegArrowAltCircleDown} value={expense} />
+            <ResumeItem title="Total" Icon={FaDollarSign} value={total} />
+        </C.Container>
+    )
+}
